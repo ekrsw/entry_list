6cd49c49f601201f0c25afb274abc42cd929e26b
ekrsw <eisuke.koresawa@gmail.com>
2024-06-05 17:11:10 +0900

    login

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
 
 from .forms import CustomUserCreationForm
+
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('accounts:signup_success')
 
     def form_valid(self, form):
         user = form.save()
         self.object = user
         return super().form_valid(form)
+
+class SignUpSuccessView(TemplateView):
+    template_name = 'accounts/signup_success.html'
+    
